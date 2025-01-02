c6d61339441cabf30c31c4c31287a243723ea6f5
Trevor Jones <jones1876@gmail.com>
2025-01-02 01:56:59 -0500

    update

--- DistanceTelemetryMod/DistanceTelemetryPlugin.cs
+++ DistanceTelemetryMod/DistanceTelemetryPlugin.cs
@@ -1,6 +1,11 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+
 using HarmonyLib;
+
+using System.Net;
+
+using TelemetryLibrary;
 
 namespace com.drowmods.DistanceTelemetryMod
 {
@@ -8,19 +13,30 @@ namespace com.drowmods.DistanceTelemetryMod
     public class DistanceTelemetryPlugin : BaseUnityPlugin
     {
         private const string MyGuid = "com.drowmods.DistanceTelemetryPlugin";
-        private const string PluginName = "DistanceTelemetryPlugin";
+        internal const string PluginName = "DistanceTelemetryPlugin";
         private const string VersionString = "1.0.0";
 
         public static readonly Harmony harmony = new Harmony(MyGuid);
         public static ManualLogSource Log;
 
+        public static void Echo(string caller, string message)
+        {
+            Log?.LogInfo(string.Format("[{0}] {1}", caller, message));
+        }
+
         public void Awake()
         {
 
             harmony.PatchAll();
-            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+
             Log = Logger;
 
+            Log.LogInfo(string.Format("{0} {1} loaded.", PluginName, VersionString));
+        }
+
+        public void Update()
+        {
+            //Logger.LogInfo(string.Format("[{0}] Update", PluginName));
         }
 
     }
@@ -29,24 +45,32 @@ namespace com.drowmods.DistanceTelemetryMod
     [HarmonyPatch(typeof(LocalPlayerControlledCar))]
     internal class LocalPlayerControlledCarPatches
     {
-        //static MemoryMappedFile mmf;
-        //static MemoryMappedViewAccessor accessor;
+
         static int _packetId = 0;
         static ManualLogSource log;
+        static UdpTelemetry<DistanceTelemetryData> udp;
 
-        //public LocalPlayerControlledCarPatches()
-        //{
-        //    log = DistanceTelemetryPlugin.Log;
-        //}
+        static LocalPlayerControlledCarPatches()
+        {
+            FileLog.Log(string.Format("[{0}] Constructor = {1}", nameof(LocalPlayerControlledCarPatches), DistanceTelemetryPlugin.Log == null));
+            log = DistanceTelemetryPlugin.Log;
+        }
 
         [HarmonyPostfix]
         [HarmonyPatch(nameof(LocalPlayerControlledCar.Start))]
         public static void Start_Postfix(LocalPlayerControlledCar __instance)
         {
-            DistanceTelemetryPlugin.Log.LogInfo("LocalPlayerControlledCar Start");
-            //mmf = MemoryMappedFile.CreateOrOpen("FPGeneric", Marshal.SizeOf(typeof(DistanceTelemetryData)));
+            if (log == null)
+            {
+                log = Logger.CreateLogSource("LocalPlayerControlledCarPatches");
+                log.LogDebug("Start_Postfix - log was null");
+            }
 
-            //accessor = mmf.CreateViewAccessor();
+            udp = new UdpTelemetry<DistanceTelemetryData>(new UdpTelemetryConfig
+            {
+                SendAddress = new IPEndPoint(IPAddress.Loopback, 12345)
+            });
+
         }
 
         [HarmonyPostfix]
@@ -57,37 +81,48 @@ namespace com.drowmods.DistanceTelemetryMod
             //var car_rb = car.GetComponent<Rigidbody>();
             //var car_logic = car.GetComponent<CarLogic>();
             //System.IO.File.WriteAllText("E:\\SteamLibrary\\SteamApps\\common\\Distance\\test.txt", "Update_Postfix");
-
+            //if (log == null)
+            //{
+            //    FileLog.Log("Update_Postfix - log was null");
+            //    //log = DistanceTelemetryPlugin.Log;
+            //    //
+            //}
 
             var data = new DistanceTelemetryData
             {
+                PacketId = _packetId,
                 X = car.transform.position.x,
                 Y = car.transform.position.y,
                 Z = car.transform.position.z,
-                Yaw = car.transform.rotation.eulerAngles.y,
-                Pitch = car.transform.rotation.eulerAngles.x,
-                Roll = car.transform.rotation.eulerAngles.z
+                Yaw = hemiCircle(car.transform.rotation.eulerAngles.y),
+                Pitch = hemiCircle(car.transform.rotation.eulerAngles.x),
+                Roll = hemiCircle(-car.transform.rotation.eulerAngles.z)
             };
 
-            //accessor?.Write(0, ref data);
+            udp.Send(data);
+
             _packetId++;
-            DistanceTelemetryPlugin.Log.LogInfo($"{_packetId} - X: {data.X:F4}, Y: {data.Y:F4}, Z: {data.Z:F4}, Yaw: {data.Yaw:F4}, Pitch: {data.Pitch:F4}, Roll: {data.Roll:F4}");
+
+            log.LogInfo($"[{_packetId}] - X: {data.X:F4}, Y: {data.Y:F4}, Z: {data.Z:F4}, Yaw: {data.Yaw:F4}, Pitch: {data.Pitch:F4}, Roll: {data.Roll:F4}");
         }
 
         [HarmonyCleanup]
         public static void CleanUp()
         {
             FileLog.Log("Cleaning Up");
-
-            //mmf?.Dispose();
-
-            //accessor?.Dispose();
+            udp?.Dispose();
         }
+
+        private static float hemiCircle(float angle)
+        {
+            return angle >= 180 ? angle - 360 : angle;
+        }
+
     }
 
-    internal struct DistanceTelemetryData
+    public struct DistanceTelemetryData
     {
-        public int DataId;
+        public int PacketId;
         public float X;
         public float Y;
         public float Z;
