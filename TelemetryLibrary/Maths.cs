619ccb0f120b25ee2f5aca9b561504f752d0b3c0
Trevor Jones <jones1876@gmail.com>
2025-01-04 22:05:50 -0500

    add assembies

---
+++ TelemetryLibrary/Maths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetryLibrary
+{
+    internal class Maths
+    {
+        public static float HemiCircle(float angle)
+        {
+            return angle >= 180 ? angle - 360 : angle;
+        }
+
+        public static float ReverseHemiCircle(float angle)
+        {
+            return angle < 0 ? 360 - angle : angle;
+        }
+    }
+}
